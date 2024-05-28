4d9b583243fa49df847ba95297dd5c07232cf200
Adi <adithiyans123@gmail.com>
2024-05-28 15:01:57 +0530

    register page isses a jwt

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 let User = require("../models/user");
 let asyncHandler = require("express-async-handler");
 let bcrypt = require("bcryptjs");
+let jwt = require("jsonwebtoken");
 
 //controller for user
 //register and login users
@@ -16,15 +17,29 @@ exports.register_user = asyncHandler(async (req,res)=>{
         if(err){
             console.log("HAshing error");
         }else{
-
+            //create use with hashed password
             let user = new User({
                 username: req.body.username,
                 p_hash: hash,
             });
 
 
-            // await user.save();
-            res.json({user: user});
+            await user.save();
+            
+
+            //generate the jwt
+
+
+            let payload = {
+                id: user._id
+            }
+
+
+            let token = jwt.sign(payload,process.env.KEY,{expiresIn: "1d"});
+
+            console.log("Token created");
+
+            res.json({user: user, token: "Bearer "+token});
         }
     }))
 })
